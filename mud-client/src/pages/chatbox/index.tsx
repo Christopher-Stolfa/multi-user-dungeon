da4cfd2924fe1eda3bc2d4577807447cf51ab941
Chris <xcstolfax@gmail.com>
2023-10-07 16:36:27 -0700

    Add a page for the route /chatbox

---
+++ mud-client/src/pages/chatbox/index.tsx
@@ -0,0 +1,10 @@
+import styles from "./Chatbox.module.scss";
+
+const Chatbox = () => {
+  return (
+    <div className={styles["root"]}>
+      <h1>Hello world!</h1>
+    </div>
+  );
+};
+export default Chatbox;
